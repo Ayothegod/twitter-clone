3e39a19d0fcf5d0773301fbffdc3ec498db53807
Ayo <heyayomideadebisi@gmail.com>
2023-12-16 19:14:25 +0100

    feat: cleanup client code

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      {/* Tweeter */}
+      <p>Time to double down on building</p>
+      <p>Time to build mad stuff</p>
+      <p>Time to build mad stuff</p>
+
+    </main>
+  );
+}
+
+export default App;
