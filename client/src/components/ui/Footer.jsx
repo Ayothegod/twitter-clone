209431662a701062bc7408562aaccc9237ee95b1
Ayo <heyayomideadebisi@gmail.com>
2023-12-17 14:28:27 +0100

    feat: fix mobile footer and add lucid icons

--- client/src/components/ui/Footer.jsx
+++ client/src/components/ui/Footer.jsx
@@ -2,7 +2,7 @@ import { Link, useLocation } from "react-router-dom";
 
 export default function Footer() {
   const location = useLocation();
-  const urlPathname = location.pathname.slice(1)
+  const urlPathname = location.pathname.slice(1);
   const links = [
     { to: "home", icon: "Home" },
     { to: "search", icon: "Search" },
@@ -11,11 +11,16 @@ export default function Footer() {
   ];
 
   return (
-    <div className=" fixed bottom-0 w-full px-2 ">
+    <div className=" fixed bottom-0 w-full px-2 sm:hidden">
       <div className="border-t border-black flex justify-between  py-2">
-
         {links.map((link, idx) => (
-          <Link to={`/${link.to}`} key={idx} className={`text-slate-300 ${urlPathname === link.to && "text-black" }`}>
+          <Link
+            to={`/${link.to}`}
+            key={idx}
+            className={`text-slate-400 font-semibold ${
+                urlPathname === link.to && "text-slate-900"
+              }`}
+          >
             {link.icon}
           </Link>
         ))}
