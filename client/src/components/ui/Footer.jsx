027b57ebdf1011ea3d1d65e7abf780ef26a4bde3
Ayo <heyayomideadebisi@gmail.com>
2023-12-17 18:47:06 +0100

    feat: move footer links outside footer

--- client/src/components/ui/Footer.jsx
+++ client/src/components/ui/Footer.jsx
@@ -1,28 +1,22 @@
 import { Link, useLocation } from "react-router-dom";
-import { Home, Search ,Bell ,Mail    } from 'lucide-react';
+import { footerLinks } from "../../utils/links";
 
 export default function Footer() {
   const location = useLocation();
   const urlPathname = location.pathname.slice(1);
-  const links = [
-    { to: "home", icon: Home },
-    { to: "search", icon: Search },
-    { to: "notification", icon: Bell },
-    { to: "message", icon: Mail },
-  ];
-{/* <Home /> */}
+
   return (
     <div className=" fixed bottom-0 w-full px-2 sm:hidden">
       <div className="border-t flex justify-between px-4 py-3">
-        {links.map((link, idx) => (
+        {footerLinks.map((link, idx) => (
           <Link
             to={`/${link.to}`}
             key={idx}
             className={`text-slate-400 font-bold ${
-                urlPathname === link.to && "text-slate-900"
-              }`}
+              urlPathname === link.to && "text-slate-900"
+            }`}
           >
-            {<link.icon/>}
+            {<link.icon />}
           </Link>
         ))}
       </div>
--- client/src/utils/links.js
+++ client/src/utils/links.js
@@ -1,14 +1,24 @@
-
 import {
-    UserRound,
-    Twitter,
-    Bookmark,
-    CreditCard,
-  } from "lucide-react";
+  UserRound,
+  Twitter,
+  Bookmark,
+  CreditCard,
+  Home,
+  Search,
+  Bell,
+  Mail,
+} from "lucide-react";
 
 export const mobileSidebarLinks = [
-    { to: "profile", icon: UserRound, text: "Profile" },
-    { to: "premium", icon: Twitter, text: "Premium" },
-    { to: "bookmark", icon: Bookmark, text: "Bookmark" },
-    { to: "monetization", icon: CreditCard, text: "Monetization" },
-  ];
+  { to: "profile", icon: UserRound, text: "Profile" },
+  { to: "premium", icon: Twitter, text: "Premium" },
+  { to: "bookmark", icon: Bookmark, text: "Bookmark" },
+  { to: "monetization", icon: CreditCard, text: "Monetization" },
+];
+
+export const footerLinks = [
+  { to: "home", icon: Home },
+  { to: "search", icon: Search },
+  { to: "notification", icon: Bell },
+  { to: "message", icon: Mail },
+];
