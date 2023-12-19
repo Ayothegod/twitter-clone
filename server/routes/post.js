1194963fa9389b4474abd64578004937265461a8
Ayo <heyayomideadebisi@gmail.com>
2023-12-19 17:53:51 +0100

    feat: all post related endpoints to post route

--- server/routes/post.js
+++ server/routes/post.js
@@ -1,6 +1,12 @@
 const express = require("express")
 const router = express.Router()
+const {createPost, getSinglePost, deletePost, getAllPostsFromUser, getAllPosts} = require("../controller/post")
+const { isAuth } = require("../middleware/session")
 
-
+router.get("/posts/:postId", isAuth, getSinglePost)
+router.get("/posts/all", isAuth, getAllPosts)
+router.get("/posts/user", isAuth, getAllPostsFromUser)
+router.post("/posts/:postId", isAuth, createPost)
+router.delete("/posts/:postId", isAuth, deletePost)
 
 module.exports = router
