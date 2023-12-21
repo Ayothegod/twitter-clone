644d318c61e75fc2b3b48f965931bbb2041f5e09
Ayo <heyayomideadebisi@gmail.com>
2023-12-21 19:07:41 +0100

    feat: built follow me route, and add followers

--- server/controller/others.js
+++ server/controller/others.js
@@ -2,33 +2,143 @@ const User = require("../models/User");
 const Post = require("../models/Post");
 const Comment = require("../models/Comment");
 
-const follow = async(req, res) => {
-    try {
-        
-
-
-        res.status(200).json({
-            success: true,
-            msg: "",
-          })
-    } catch (error) {
-        console.log(error);
-        res.status(500);
-        throw new Error("Something went wrong");
+const follower = async (req, res) => {
+  try {
+    const { username } = req.params;
+    const { followerUsername } = req.body;
+    if (!username || !followerUsername) {
+      return res
+        .status(401)
+        .json("you must provide username and followerUsername");
     }
-}
-
-const bookmark = async(req, res) => {
-    try {
-        res.status(200).json({
-            success: true,
-            msg: "",
-          })
-    } catch (error) {
-        console.log(error);
-        res.status(500);
-        throw new Error("Something went wrong");
+    if (username === followerUsername) {
+      return res.status(400).json(null);
     }
-}
 
-module.exports = {follow, bookmark}
+    // find my account
+    const myAccount = await User.findOne({ username });
+
+    // check if followerUsername is included in followers array, if yes unfollow
+    if (myAccount.followers.includes(followerUsername)) {
+      myAccount.followers.pull(followerUsername);
+      await myAccount.save();
+      return res.status(401).json(`${followerUsername} has unfollowed you`);
+    }
+
+    // follow me
+    myAccount.followers.push(followerUsername);
+    await myAccount.save();
+
+    res.status(200).json({
+      success: true,
+      msg: `${followerUsername} has followed you`,
+      myAccount,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500);
+    throw new Error("Something went wrong");
+  }
+};
+
+const following = async (req, res) => {
+  try {
+    const { username } = req.params;
+    const { followerUsername } = req.body;
+    if (!username || !followerUsername) {
+      return res
+        .status(401)
+        .json("you must provide username and followerUsername");
+    }
+    if (username === followerUsername) {
+      return res.status(400).json(null);
+    }
+
+    // find my account
+    const myAccount = await User.findOne({ username });
+
+    // check if followerUsername is included in followers array, if yes unfollow
+    if (myAccount.followers.includes(followerUsername)) {
+      myAccount.followers.pull(followerUsername);
+      await post.save();
+      return res.status(401).json(`${followerUsername} has unfollowed you`);
+    }
+
+    // follow me
+    myAccount.followers.push(followerUsername);
+    await myAccount.save();
+
+    res.status(200).json({
+      success: true,
+      msg: `${followerUsername} has followed you`,
+      myAccount,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500);
+    throw new Error("Something went wrong");
+  }
+};
+
+const bookmark = async (req, res) => {
+  try {
+    res.status(200).json({
+      success: true,
+      msg: "",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500);
+    throw new Error("Something went wrong");
+  }
+};
+
+module.exports = { follower, following, bookmark };
+
+// {
+//     "success": true,
+//     "msg": "user created successfully",
+//     "user": {
+//       "fullname": "Toheeb",
+//       "email": "",
+//       "username": "Toheeb",
+//       "password": "$2b$10$Qdo..FY2g75xhtVhnFerN.yLgtad0k99JeHkcXaIGyDrEpA0p54eK",
+//       "followers": [],
+//       "following": [],
+//       "profilePhoto": "",
+//       "coverPhoto": "",
+//       "location": "",
+//       "profession": "",
+//       "bio": "",
+//       "website": "",
+//       "postsId": [],
+//       "_id": "658477bb87a9582f09e0cf68",
+//       "createdAt": "2023-12-21T17:37:00.025Z",
+//       "updatedAt": "2023-12-21T17:37:00.025Z",
+//       "__v": 0
+//     }
+//   }
+
+// {
+//     "success": true,
+//     "msg": "user created successfully",
+//     "user": {
+//       "fullname": "Aiiomide",
+//       "email": "",
+//       "username": "Aiiomide",
+//       "password": "$2b$10$b13sTsp1fyK5gU7CtKDhYe3YjBhM/vSV9TptAriPIdO5sVj97Z.yi",
+//       "followers": [],
+//       "following": [],
+//       "profilePhoto": "",
+//       "coverPhoto": "",
+//       "location": "",
+//       "profession": "",
+//       "bio": "",
+//       "website": "",
+//       "postsId": [],
+//       "_id": "658477e4f8c0780939d4cf03",
+//       "createdAt": "2023-12-21T17:37:40.926Z",
+//       "updatedAt": "2023-12-21T17:37:40.926Z",
+//       "__v": 0
+//     }
+//   }
--- server/routes/profile.js
+++ server/routes/profile.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const { isAuth } = require("../middleware/session");
 const { getUserProfile, updateUserProfile, deleteUserProfile } = require("../controller/profile");
-const { follow } = require("../controller/others");
+const { follower, following, } = require("../controller/others");
 const router = express.Router();
 
 router.get("/profile/:username", isAuth, getUserProfile)
 router.patch("/profile/update/:username", isAuth, updateUserProfile)
 router.delete("/profile/delete/:username", isAuth, deleteUserProfile)
 
-router.post("/profile/:username/follow", isAuth, follow)
+router.post("/profile/:username/follow-me", isAuth, follower)
+router.post("/profile/:username/i-follow", isAuth, following)
 
 module.exports = router;
